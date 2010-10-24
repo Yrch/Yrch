76c251d217a6dfc2a86b54559d860540256797f1
Christophe COEVOET <stof@notk.org>
2010-10-24 18:10:54 +0200

    Correction de la classe User

--- src/Application/YrchBundle/Entity/User.php
+++ src/Application/YrchBundle/Entity/User.php
@@ -7,25 +7,9 @@ use Bundle\DoctrineUserBundle\Entity\User as BaseUser;
 /**
  * Application\YrchBundle\Entity\User
  *
- * @Entity
+ * @orm:Entity
  */
 class User extends BaseUser
 {
-    /**
-     * @var integer $id
-     *
-     * @orm:Column(name="id", type="integer")
-     * @orm:Id
-     * @orm:GeneratedValue(strategy="AUTO")
-     */
-    protected $id;
-
-    /**
-     *
-     * @var string
-     *
-     * @orm:Column(name="username", type="string(255)")
-     */
-    protected $username;
-
+    
 }
