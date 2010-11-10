001e12b4edbd8a924fcdc3e8b5ae28a13ca57eaf
Christophe COEVOET <stof@notk.org>
2010-11-10 01:22:01 +0100

    Modified SiteTranslation to use DoctrineExtensionsBundle\Entity\AbstractTranslationEntity

--- src/Application/YrchBundle/Entity/SiteTranslation.php
+++ src/Application/YrchBundle/Entity/SiteTranslation.php
@@ -2,55 +2,14 @@
 
 namespace Application\YrchBundle\Entity;
 
+use Bundle\DoctrineExtensionsBundle\Entity\AbstractTranslationEntity;
+
 /**
  * @orm:Table(name="site_translations", indexes={
  *      @orm:index(name="site_translation_idx", columns={"locale", "entity", "foreign_key", "field"})
  * })
  * @orm:Entity(repositoryClass="DoctrineExtensions\Translatable\Repository\TranslationRepository")
  */
-class SiteTranslation
+class SiteTranslation extends AbstractTranslationEntity
 {
-    /**
-     * @var integer $id
-     *
-     * @orm:Column(name="id", type="integer")
-     * @orm:Id
-     * @orm:GeneratedValue(strategy="IDENTITY")
-     */
-    private $id;
-
-    /**
-     * @var string $locale
-     *
-     * @orm:Column(name="locale", type="string", length=8)
-     */
-    private $locale;
-
-    /**
-     * @var string $entity
-     *
-     * @orm:Column(name="entity", type="string", length=255)
-     */
-    private $entity;
-
-    /**
-     * @var string $field
-     *
-     * @orm:Column(name="field", type="string", length=32)
-     */
-    private $field;
-
-    /**
-     * @var string $foreignKey
-     *
-     * @orm:Column(name="foreign_key", type="string", length="64")
-     */
-    private $foreignKey;
-
-    /**
-     * @var text $content
-     *
-     * @orm:Column(name="content", type="text", nullable=true)
-     */
-    private $content;
 }
