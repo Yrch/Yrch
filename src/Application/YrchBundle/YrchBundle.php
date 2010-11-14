d1ae360c4dbc9403e5a20837a3d1548ad270391a
Christophe COEVOET <stof@notk.org>
2010-11-14 18:28:30 +0100

    Removed the useless reference to DoctrineExtensions namespace

--- src/Application/YrchBundle/YrchBundle.php
+++ src/Application/YrchBundle/YrchBundle.php
@@ -5,7 +5,6 @@ namespace Application\YrchBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
-use DoctrineExtensions\Translatable\TranslationListener;
 
 class YrchBundle extends Bundle
 {
