103b2b39f6876efd5bc47f3a1020c32972683b0c
Christophe Coevoet <stof@notk.org>
2010-11-19 12:18:05 +0100

    Added a command to generate groups and permissions

---
+++ src/Application/YrchBundle/Command/GenerateGroupsCommand.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Application\YrchBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Output\Output;
+use Bundle\DoctrineUserBundle\Entity\Group;
+use Bundle\DoctrineUserBundle\Entity\Permission;
+
+/**
+ * GenerateGroupsCommand.
+ */
+class GenerateGroupsCommand extends Command
+{
+    /**
+     * @see Command
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('yrch:generate:groups')
+            ->setDescription('Generate the groups.')
+            ->setHelp(<<<EOT
+The <info>yrch:generate:groups</info> command creates the groups needed by Ych!:
+
+  <info>php app/console yrch:generate:groups</info>
+
+EOT
+        );
+    }
+
+    /**
+     * @see Command
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $groupRepo = $this->container->get('doctrine_user.repository.group');
+        $groupClass = $groupRepo->getObjectClass();
+        // Admin
+        $adminGroup = $groupRepo->findOneByName('Admin');
+        if ($adminGroup === null){
+            $adminGroup = new $groupClass();
+            $adminGroup->setName('Admin');
+            $adminGroup->setDescription('Administrator');
+            $output->writeln(sprintf('Created group <comment>%s</comment>', $adminGroup->getName()));
+        }
+        $adminGroup->addPermission($this->addPermission('admin', 'admin access', $output));
+        $adminGroup->addPermission($this->addPermission('moderator', 'moderator access', $output));
+        $groupRepo->getObjectManager()->persist($adminGroup);
+        // Moderator
+        $moderatorGroup = $groupRepo->findOneByName('Moderator');
+        if ($moderatorGroup === null){
+            $moderatorGroup = new $groupClass();
+            $moderatorGroup->setName('Moderator');
+            $moderatorGroup->setDescription('Moderator');
+            $output->writeln(sprintf('Created group <comment>%s</comment>', $moderatorGroup->getName()));
+        }
+        $moderatorGroup->addPermission($this->addPermission('moderator', 'moderator access', $output));
+        $groupRepo->getObjectManager()->persist($moderatorGroup);
+        $groupRepo->getObjectManager()->flush();
+    }
+
+    protected function addPermission($name, $description, OutputInterface $output)
+    {
+        $permissionRepo = $this->container->get('doctrine_user.repository.permission');
+        $permissionClass = $permissionRepo->getObjectClass();
+        $permission = $permissionRepo->findOneByName($name);
+        if ($permission === null){
+            $permission = new $permissionClass();
+            $permission->setName($name);
+            $permission->setDescription($description);
+            $permissionRepo->getObjectManager()->persist($permission);
+            $permissionRepo->getObjectManager()->flush();
+            $output->writeln(sprintf('Created permission <comment>%s</comment>', $permission->getName()));
+        }
+        return $permission;
+    }
+}
