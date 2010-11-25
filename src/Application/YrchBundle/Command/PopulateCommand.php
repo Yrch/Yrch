979edc682e23b1c170e8f8f40474276b313a8741
Christophe Coevoet <stof@notk.org>
2010-11-25 14:14:01 +0100

    Renamed yrch:generate:groups to yrch:populate and added the creation of the special user

--- src/Application/YrchBundle/Command/PopulateCommand.php
+++ src/Application/YrchBundle/Command/PopulateCommand.php
@@ -6,11 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Output\Output;
+use Application\YrchBundle\Entity\User;
 
 /**
  * GenerateGroupsCommand.
  */
-class GenerateGroupsCommand extends Command
+class PopulateCommand extends Command
 {
     /**
      * @see Command
@@ -18,12 +19,13 @@ class GenerateGroupsCommand extends Command
     protected function configure()
     {
         $this
-            ->setName('yrch:generate:groups')
-            ->setDescription('Generate the groups.')
+            ->setName('yrch:populate')
+            ->setDescription('Generate the groups and permissions and the special user.')
             ->setHelp(<<<EOT
-The <info>yrch:generate:groups</info> command creates the groups needed by Ych!:
+The <info>yrch:populate</info> command creates the groups,
+the permissions and the special user needed by Yrch!:
 
-  <info>php app/console yrch:generate:groups</info>
+  <info>php app/console yrch:populate</info>
 
 EOT
         );
@@ -34,7 +36,7 @@ EOT
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('Generating permissions and groups');
+        $output->writeln('Populating database');
         $groupRepo = $this->container->get('doctrine_user.repository.group');
         $groupClass = $groupRepo->getObjectClass();
         // Admin
@@ -62,6 +64,22 @@ EOT
         }
         $moderatorGroup->addPermission($this->addPermission('moderator', 'moderator access', $output));
         $groupRepo->getObjectManager()->persist($moderatorGroup);
+        // Special user
+        $userRepo = $this->container->get('doctrine_user.repository.user');
+        if (null === $userRepo->findOneByUsername($this->container->getParameter('yrch.special_user.username'))){
+            $specialUser = new User();
+            $specialUser->setUsername($this->container->getParameter('yrch.special_user.username'));
+            $specialUser->setNick($this->container->getParameter('yrch.special_user.nick'));
+            $specialUser->setEmail($this->container->getParameter('yrch.special_user.email'));
+            $specialUser->setPreferedLocale($this->container->getParameter('session.default_locale'));
+            $specialUser->setPassword(md5(uniqid() . rand(100000, 999999)));
+            $specialUser->lock();
+            $specialUser->setIsActive(true);
+            $userRepo->getObjectManager()->persist($specialUser);
+            if ($output->getVerbosity() == Output::VERBOSITY_VERBOSE){
+                $output->writeln(sprintf('Created user <comment>%s</comment>', $specialUser->getNick()));
+            }
+        }
         $groupRepo->getObjectManager()->flush();
     }
 
