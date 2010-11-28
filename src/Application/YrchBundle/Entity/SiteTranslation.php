cb17281369f54f369965323eda58004e461d132f
Christophe Coevoet <stof@notk.org>
2010-11-28 22:56:36 +0100

    Updated indexes to match the DoctrineExtensionsBundle update

--- src/Application/YrchBundle/Entity/SiteTranslation.php
+++ src/Application/YrchBundle/Entity/SiteTranslation.php
@@ -5,9 +5,15 @@ namespace Application\YrchBundle\Entity;
 use Bundle\DoctrineExtensionsBundle\Entity\AbstractTranslation;
 
 /**
- * @orm:Table(name="site_translations", indexes={
- *      @orm:index(name="site_translation_idx", columns={"locale", "entity", "foreign_key", "field"})
- * })
+ * @orm:Table(
+ *          name="site_translations",
+ *          indexes={@orm:index(name="site_translations_lookup_idx", columns={
+ *             "locale", "entity", "foreign_key"
+ *         })},
+ *         uniqueConstraints={@orm:UniqueConstraint(name="site_lookup_unique_idx", columns={
+ *             "locale", "entity", "foreign_key", "field"
+ *         })}
+ * )
  * @orm:Entity(repositoryClass="Gedmo\Translatable\Repository\TranslationRepository")
  */
 class SiteTranslation extends AbstractTranslation
