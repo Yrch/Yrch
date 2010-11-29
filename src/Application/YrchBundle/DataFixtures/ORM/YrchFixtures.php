24b99c8378f1ce10bbcba58077376cb6149b283e
Christophe Coevoet <stof@notk.org>
2010-11-29 22:39:20 +0100

    Added User and Site fixtures

--- src/Application/YrchBundle/DataFixtures/ORM/YrchFixtures.php
+++ src/Application/YrchBundle/DataFixtures/ORM/YrchFixtures.php
@@ -5,6 +5,8 @@ namespace Application\YrchBundle\DataFixtures\ORM;
 use Doctrine\ORM\EntityManager;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Application\YrchBundle\Entity\Category;
+use Application\YrchBundle\Entity\Site;
+use Application\YrchBundle\Entity\User;
 
 class YrchFixtures implements FixtureInterface
 {
@@ -14,6 +16,9 @@ class YrchFixtures implements FixtureInterface
         $rootNodes = $categoryRepo->children(null, true);
         $rootCategory = $rootNodes[0];
         $categories = array();
+        $users = array();
+        $sites = array();
+        // Categories
         for ($i = 0; $i < 5; $i++) {
             $cat = $this->createCategory($i, $rootCategory);
             $categories[$i] = $cat;
@@ -37,6 +42,21 @@ class YrchFixtures implements FixtureInterface
             $manager->persist($categories[$index]);
         }
         $manager->flush();
+        // Users
+        for ($i = 0; $i < 5; $i++) {
+            $user = $this->createUser($i);
+            $users[$i] = $user;
+            $manager->persist($user);
+        }
+        // Sites
+        for ($i = 0; $i < 15; $i++) {
+            $site = $this->createSite($i, $users[$i % 5]);
+            $site->addCategory($categories[$i % 10]);
+            $site->addCategory($categories[($i+2) % 7]);
+            $sites[$i] = $site;
+            $manager->persist($site);
+        }
+        $manager->flush();
     }
 
     /**
@@ -56,4 +76,40 @@ class YrchFixtures implements FixtureInterface
         return $cat;
     }
 
+    /**
+     * Create a new user
+     *
+     * @param int $i
+     * @return User
+     */
+    protected function createUser($i)
+    {
+        $user = new User();
+        $user->setUsername('user_'.$i);
+        $user->setNick('User '.$i);
+        $user->setEmail('user'.$i.'@example.org');
+        $user->setPassword('passwd'.$i);
+        $user->setPreferedLocale('fr');
+        return $user;
+    }
+
+    /**
+     * Create a new site
+     *
+     * @param int $i
+     * @param User $superOwner
+     * @return Site
+     */
+    protected function createSite($i, User $superOwner)
+    {
+        $site = new Site();
+        $site->setTranslatableLocale('en');
+        $site->setUrl('http://'.$i.'example.org');
+        $site->setName('Site '.$i);
+        $site->setDescription('test site number '.$i);
+        $site->setLanguages(array('en', 'fr'));
+        $site->setCountries(array('US', 'CA'));
+        $site->setSuperOwner($superOwner);
+        return $site;
+    }
 }
