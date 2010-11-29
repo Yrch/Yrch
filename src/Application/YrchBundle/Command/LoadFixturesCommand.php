27e103481381cebff8cbe863151dc0f909967ffc
Christophe Coevoet <stof@notk.org>
2010-11-30 00:05:54 +0100

    Removed unneeded use statements and attributes

--- src/Application/YrchBundle/Command/LoadFixturesCommand.php
+++ src/Application/YrchBundle/Command/LoadFixturesCommand.php
@@ -3,46 +3,17 @@
 namespace Application\YrchBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Output\Output;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Input\StringInput;
 use Doctrine\Common\DataFixtures\Purger\ORMPurger;
-use Application\YrchBundle\Entity\User;
-use Application\YrchBundle\Entity\Site;
-use Application\YrchBundle\Entity\Review;
-use Application\YrchBundle\Entity\Category;
-use Application\YrchBundle\Entity\SiteTemp;
 
 /**
  * MigrateCommand.
  */
 class LoadFixturesCommand extends Command
 {
-    /**
-     * This array contains key/value couples as <old user_id> => User
-     *
-     * @var array
-     */
-    protected $users = array ();
-
-    /**
-     * This array contains key/value couples as <old site_id> => Site
-     *
-     * @var array
-     */
-    protected $sites = array ();
-
-    /**
-     * This array contains key/value couples as <old cat_id> => Category
-     *
-     * @var array
-     */
-    protected $categories = array ();
-
     /**
      * @see Command
      */
@@ -69,6 +40,7 @@ EOT
     {
         $purger = new ORMPurger($this->container->get('doctrine.orm.entity_manager'));
         $purger->purge();
+
         $string_input = new StringInput('yrch:populate');
         $application = new Application($this->application->getKernel());
         $application->setAutoExit(false);
