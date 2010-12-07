f4de52b1b2f3e6aea9dca052ec2ea7dae7a3496a
Christophe Coevoet <stof@notk.org>
2010-12-07 12:40:27 +0100

    Added a test to intercept the exception raised when no correspondance is found for the language
    It seems this is a bug in the Request method

--- src/Application/YrchBundle/Controller/MainController.php
+++ src/Application/YrchBundle/Controller/MainController.php
@@ -8,7 +8,11 @@ class MainController extends Controller
 {
     public function indexAction()
     {
-        $locale = $this->get('request')->getPreferredLanguage($this->container->getParameter('yrch.languages'));
+        try {
+            $locale = $this->get('request')->getPreferredLanguage($this->container->getParameter('yrch.languages'));
+        } catch (\Exception $e) {
+            $locale = $this->container->getParameter('session.default_locale');
+        }
         return $this->redirect($this->generateUrl($this->get('templating.helper.locale')->getRoute("localized_homepage", $locale)));
     }
 }
