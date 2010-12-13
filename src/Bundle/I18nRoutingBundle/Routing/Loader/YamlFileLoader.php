8be950925e806dd9e43369cde7ca670fc5a24b46
Christophe Coevoet <stof@notk.org>
2010-12-13 20:59:02 +0100

    Updated I18nRoutingBundle

--- src/Bundle/I18nRoutingBundle/Routing/Loader/YamlFileLoader.php
+++ src/Bundle/I18nRoutingBundle/Routing/Loader/YamlFileLoader.php
@@ -12,13 +12,14 @@ class YamlFileLoader extends BaseYamlFileLoader
     /**
      * Loads a Yaml file.
      *
-     * @param  string $file A Yaml file path
+     * @param string $file A Yaml file path
+     * @param string $type The resource type
      *
      * @return RouteCollection A RouteCollection instance
      *
      * @throws \InvalidArgumentException When route can't be parsed
      */
-    public function load($file)
+    public function load($file, $type = null)
     {
         $path = $this->findFile($file);
 
@@ -27,11 +28,22 @@ class YamlFileLoader extends BaseYamlFileLoader
         $collection = new RouteCollection();
         $collection->addResource(new FileResource($path));
 
+        // empty file
+        if (null === $config) {
+            $config = array();
+        }
+
+        // not an array
+        if (!is_array($config)) {
+            throw new \InvalidArgumentException(sprintf('The file "%s" must contain a YAML array.', $file));
+        }
+
         foreach ($config as $name => $config) {
             if (isset($config['resource'])) {
+                $type = isset($config['type']) ? $config['type'] : null;
                 $prefix = isset($config['prefix']) ? $config['prefix'] : null;
                 $this->currentDir = dirname($path);
-                $collection->addCollection($this->import($config['resource']), $prefix);
+                $collection->addCollection($this->import($config['resource'], $type), $prefix);
             } elseif (isset($config['pattern']) || isset($config['locales'])) {
                 $this->parseRoute($collection, $name, $config, $path);
             } else {
