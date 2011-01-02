5ab256424fbe3021a002297961bdb5bfc819811b
Christophe Coevoet <stof@notk.org>
2011-01-02 17:31:09 +0100

    Changed DQL query to make them more readable

--- src/Application/YrchBundle/Repository/SiteRepository.php
+++ src/Application/YrchBundle/Repository/SiteRepository.php
@@ -26,7 +26,7 @@ class SiteRepository extends EntityRepository
     public function findByCategory(Category $category)
     {
         $dql = "SELECT s, c
-            FROM Application\YrchBundle\Entity\Site s
+            FROM YrchBundle:Site s
             JOIN s.categories c
             WHERE s.status = :status
             AND :category MEMBER OF s.categories
@@ -45,7 +45,7 @@ class SiteRepository extends EntityRepository
     public function findSelectioned()
     {
         $dql = "SELECT s, c
-            FROM Application\YrchBundle\Entity\Site s
+            FROM YrchBundle:Site s
             JOIN s.categories c
             WHERE s.status = :status
             AND s.selection = :selection
@@ -65,11 +65,11 @@ class SiteRepository extends EntityRepository
     public function getUpdatedAverageScore(Site $site)
     {
         $dql = "SELECT AVG(r.score)
-            FROM Application\YrchBundle\Entity\Review r
+            FROM YrchBundle:Review r
             WHERE r.site=?1
             AND EXISTS (
                 SELECT COUNT(e.id)
-                FROM Application\YrchBundle\Entity\Review e
+                FROM YrchBundle:Review e
                 WHERE e.site=?2
                 AND e.owner=r.owner
                 AND e.status='ok'
